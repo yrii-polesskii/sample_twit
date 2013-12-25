bb48cc65f97bb8d92a20e6b9836e6e5bd169845a
Yrii <asbim@mail.ru>
2013-12-25 20:59:53 +0700

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ SampleTwit::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  ActiveModel::SecurePassword.min_cost = true
 end
