0941afbaa23e4c9df49c60890de2dd1c37d7c6f1
Yrii <asbim@mail.ru>
2013-12-26 19:30:40 +0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }                                    
   has_secure_password
   before_save { email.downcase! }
+  before_create :create_remember_token
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    name     "Rails Tutorial"
+    email    "example@railstutorial.org"
     password "foobar"
     password_confirmation "foobar"
   end
